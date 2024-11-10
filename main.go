cac0aae76fecfbd7f0fdbfb52f59072a7163804d
user-karthikeyan <karthikeyan20032003@gmail.com>
2024-11-10 21:32:22 +0530

    Added support for get and set commands

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"encoding/gob"
+	"bufio"
 	"flag"
 	"fmt"
 	"net"
+	"strings"
 )
+
+type Datablock struct {
+	key        string
+	data_block string
+	flags      uint16
+	expiry     int
+	byte_count int
+}
 
 var port *string = flag.String("port", "11211", "Server will be running on the given port if specified defaults to 1121")
 var ip string = "localhost"
+var cache map[string]Datablock = make(map[string]Datablock)
 
 func start_server() {
 	address := ip + ":" + *port
@@ -32,40 +42,74 @@ func service_client(Listener net.Listener) {
 			continue
 		} else {
 			fmt.Println("Connected to client")
-			handle_connection(Connection)
+			go handle_connection(Connection)
 		}
 	}
 }
 
 func handle_connection(Connection net.Conn) {
-	for {
-		var command string
+	var (
+		command string
+		block   string
+		error   error
+	)
 
-		error := gob.NewDecoder(Connection).Decode(&command)
+	reader := bufio.NewReader(Connection)
+	writer := bufio.NewWriter(Connection)
+
+	for {
+		command, error = reader.ReadString('\n')
+		command = strings.TrimRight(command, "\r\n")
+
+		if !strings.HasPrefix(command, "get") {
+			block, error = reader.ReadString('\n')
+			block = strings.TrimRight(block, "\r\n")
+		}
 
 		if error != nil {
 			fmt.Println("Error:", error)
 			return
 		} else {
-			fmt.Println(command)
-			parseCommand(command)
+			result := parseCommand(command, block)
+			writer.WriteString(result)
+			writer.Flush()
 		}
 	}
 }
 
-func parseCommand(command string) {
-
+func parseCommand(command string, data_block string) string {
 	var (
-		name       string
-		key        string
-		flags      int
-		expiry     int
-		byte_count int
-		data_block string
+		block  Datablock
+		result string
 	)
 
-	fmt.Sscanf(command, "%s %s %d %d %d \r\n%s\r\n", name, key, flags, expiry, byte_count, data_block)
-	fmt.Println(name, key, flags, expiry, byte_count, data_block)
+	block.data_block = data_block
+	name, args, _ := strings.Cut(command, " ")
+
+	if name == "set" {
+		fmt.Sscanf(args, "%v %v %v %v %v", &block.key, &block.flags, &block.expiry, &block.byte_count)
+		result = putBlock(block)
+	} else {
+		key, _, _ := strings.Cut(args, " ")
+		result = getBlock(key)
+	}
+
+	return result
+}
+
+func getBlock(key string) string {
+	value, present := cache[key]
+
+	if present {
+		return fmt.Sprintf("VALUE %v %v %v\r\n%v\r\nEND\r\n", value.key, value.flags, value.byte_count, value.data_block)
+	} else {
+		return "END\r\n"
+	}
+}
+
+func putBlock(block Datablock) string {
+	cache[block.key] = block
+	return "STORED\r\n"
 }
 
 func main() {
